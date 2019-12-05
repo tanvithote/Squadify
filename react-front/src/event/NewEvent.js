b957736dad776c48b790fceaab2682389dc1ac51
Yuhan Zeng <yuhzeng@iu.edu>
2019-12-05 16:46:45 -0500

    Fix the event time that showing current date, not the picked date

--- react-front/src/event/NewEvent.js
+++ react-front/src/event/NewEvent.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import { isAuthenticated } from "../auth";
 import { create, attendEvent } from "./apiEvent";
-import  {singleGroup} from "../group/apiGroup";
+import { singleGroup } from "../group/apiGroup";
 import { Redirect } from "react-router-dom";
-import 'date-fns';
+import "date-fns";
 import DefaultPost from "../images/tea.jpg";
-import Grid from '@material-ui/core/Grid';
-import {MuiPickersUtilsProvider,KeyboardTimePicker,KeyboardDatePicker } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
+import Grid from "@material-ui/core/Grid";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 class NewEvent extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -20,21 +23,21 @@ class NewEvent extends Component {
       photo: "",
       error: "",
       user: {},
-      group:{},
+      group: {},
       tags: [],
       fileSize: 0,
       eventdate: new Date(),
       starttime: new Date(),
-      endtime: new Date (),
+      endtime: new Date(),
       redirectToGroups: false
     };
   }
 
   componentDidMount() {
     this.eventData = new FormData();
-    this.setState({ user: isAuthenticated().user});
+    this.setState({ user: isAuthenticated().user });
     this.groupId = this.props.match.params.groupId;
-    this.setState({ group: this.groupId});
+    this.setState({ group: this.groupId });
     singleGroup(this.groupId).then(data => {
       if (data.error) {
         console.log(data.error);
@@ -86,24 +89,32 @@ class NewEvent extends Component {
       passInValue === "location" ? event.target.value.toLowerCase() : "";
     this.eventData.set(passInValue, value);
     this.setState({ [passInValue]: value, fileSize: fileSize });
-
-
   };
 
-  handleDateChange=date=>{
-    this.setState({eventdate:date});
-    this.eventData.set("eventdate",date);
-  }
+  handleDateChange = date => {
+    this.setState({ eventdate: date });
+    this.eventData.set("eventdate", date);
+  };
 
-  handleStartTimeChange=time=>{
-    this.setState({starttime:time});
-    this.eventData.set("starttime",time);
-  }
+  handleStartTimeChange = time => {
+    // console.log(new Date(this.eventData.get("eventdate")));
+    // console.log(time.toString().substring(4, 15));
+    let dateString = this.eventData.get("eventdate").toString().substring(4, 15);
+    let timeString1 = time.toString().substring(0, 4).concat(dateString);
+    let timeString2 = time.toString().substring(15);
+    let eventTime = timeString1.concat(timeString2);
+    this.setState({ starttime: new Date(eventTime) });
+    this.eventData.set("starttime", new Date(eventTime));
+  };
 
-  handleEndTimeChange=time=>{
-    this.setState({endtime:time});
-    this.eventData.set("endtime",time);
-  }
+  handleEndTimeChange = time => {
+    let dateString = this.eventData.get("eventdate").toString().substring(4, 15);
+    let timeString1 = time.toString().substring(0, 4).concat(dateString);
+    let timeString2 = time.toString().substring(15);
+    let eventTime = timeString1.concat(timeString2);
+    this.setState({ endtime: new Date(eventTime) });
+    this.eventData.set("endtime", new Date(eventTime));
+  };
 
   clickSubmit = event => {
     event.preventDefault();
@@ -139,9 +150,16 @@ class NewEvent extends Component {
       });
     }
   };
-  newEventForm = (name, location, tags, description ,starttime,endtime,eventdate) => (
-
-    <form >
+  newEventForm = (
+    name,
+    location,
+    tags,
+    description,
+    starttime,
+    endtime,
+    eventdate
+  ) => (
+    <form>
       <div className="form-group">
         <label className="text-muted">Name</label>
         <input
@@ -171,44 +189,42 @@ class NewEvent extends Component {
         ></textarea>
       </div>
       <div className="form-group">
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Grid container >
-        <KeyboardDatePicker
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="eventdate"
-          label="Event Date "
-          value={eventdate}
-          onChange={this.handleDateChange}
-          KeyboardButtonProps={{
-            "aria-label": "change date"
-          }}
-        />
-        <KeyboardTimePicker
-
-          margin="normal"
-          id="time-picker"
-          label="Start Time For Event "
-          value={starttime}
-          onChange={this.handleStartTimeChange}
-          KeyboardButtonProps={{
-            "aria-label": "change time"
-          }}
-        />
-        <KeyboardTimePicker
-
-          margin="normal"
-          id="time-picker"
-          label="End Time For Event"
-          value={endtime}
-          onChange={this.handleEndTimeChange}
-          KeyboardButtonProps={{
-            "aria-label": "change time"
-          }}
-        />
-      </Grid>
-    </MuiPickersUtilsProvider>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container>
+            <KeyboardDatePicker
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="eventdate"
+              label="Event Date "
+              value={eventdate}
+              onChange={this.handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date"
+              }}
+            />
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="Start Time For Event "
+              value={starttime}
+              onChange={this.handleStartTimeChange}
+              KeyboardButtonProps={{
+                "aria-label": "change time"
+              }}
+            />
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="End Time For Event"
+              value={endtime}
+              onChange={this.handleEndTimeChange}
+              KeyboardButtonProps={{
+                "aria-label": "change time"
+              }}
+            />
+          </Grid>
+        </MuiPickersUtilsProvider>
       </div>
       <div className="form-group">
         <label className="text-muted">Tags</label>
@@ -264,10 +280,20 @@ class NewEvent extends Component {
 
     return (
       <div className="container">
-        <h2 className="mt-5 mb-5">Create a new Event for Group {this.state.group.name}</h2>
+        <h2 className="mt-5 mb-5">
+          Create a new Event for Group {this.state.group.name}
+        </h2>
         {error && <div className="alert alert-danger">{error}</div>}
 
-        {this.newEventForm(name, location, tags, description,starttime,endtime,eventdate)}
+        {this.newEventForm(
+          name,
+          location,
+          tags,
+          description,
+          starttime,
+          endtime,
+          eventdate
+        )}
       </div>
     );
   }
