db997ccc3f96f86335903bb3f7dd16a31ca061b8
Yuhan Zeng <yuhzeng@iu.edu>
2019-12-11 13:06:08 -0500

    Fix menu not showing name when log in for the first time

--- react-front/src/core/Home.js
+++ react-front/src/core/Home.js
@@ -1,44 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Menu from "./Menu";
-import image from '../images/home2 (1).png';
-import image2 from '../images/home.png';
+import image from "../images/home2 (1).png";
+import image2 from "../images/home.png";
+import { isAuthenticated } from "../auth";
 
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reloadPage: false
+    };
+  }
 
+  componentDidMount = () => {
+    if (!isAuthenticated()) {
+      this.setState({ reloadPage: true });
+    }
+  };
 
-const Home = () => (
-  <>
-  <div>
-    <Menu/>
-    </div>
-    <div
-      style={{
-        backgroundImage: `url("${image}")`,
-        height: "100%", 
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "70vh"
-      }}
-    >
+  render() {
+    const { reloadPage } = this.state;
+    if (reloadPage) {
+      window.location.reload();
+    }
 
-    </div>
-    <div
-      style={{
-        backgroundImage: `url("${image2}")`,
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "20vh"
-      }}
-    >
-    {/* <h2 m class="heroPrimary">Find your next event</h2> */}
-    {/* backgroundImage: `url("${image}")`; */}
-    
-    
-   </div>
-  </>
-);
+    return (
+      <>
+        <div>
+          <Menu />
+        </div>
+        <div
+          style={{
+            backgroundImage: `url("${image}")`,
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "70vh"
+          }}
+        ></div>
+        <div
+          style={{
+            backgroundImage: `url("${image2}")`,
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "20vh"
+          }}
+        >
+          {/* <h2 m class="heroPrimary">Find your next event</h2> */}
+          {/* backgroundImage: `url("${image}")`; */}
+        </div>
+      </>
+    );
+  }
+}
 
 export default Home;
