045244fcdf38f7058f22c7a81111aeb5dc7c3a48
Yuhan Zeng <yuhzeng@iu.edu>
2019-12-12 18:58:27 -0500

    Commit

--- react-front/src/core/Home.js
+++ react-front/src/core/Home.js
@@ -1,23 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Menu from "./Menu";
-import image from '../images/home2 (1).png';
-import image2 from '../images/home.png';
+import image from "../images/home2 (1).png";
+import image2 from "../images/home.png";
 import { Container } from "@material-ui/core";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { isAuthenticated } from "../auth";
 import DefaultPost from "../images/tea.jpg";
 
-class Home extends Component{
-
-  constructor(){
+class Home extends Component {
+  constructor() {
     super();
-    this.state ={paramlist: []};
-    reloadPage: false
-}
-  
-  
+    this.state = { paramlist: [], reloadPage: false };
+  }
+
   componentDidMount() {
     const { reloadPage } = this.state;
     if (reloadPage) {
@@ -26,23 +23,22 @@ class Home extends Component{
 
     const token = isAuthenticated().token;
     const user = isAuthenticated().user;
-    
-    console.log(user);
-    fetch(`${process.env.REACT_APP_API_URL}/groupsbyTags`,{
-      method: 'POST',
-      body: JSON.stringify(user),
-      headers: {"Content-Type": "application/json"}
-    })
-      .then(res =>  res.json())
-      .then((jsonData) =>{
-          //console.log(jsonData);
-          this.setState({groups: jsonData})
-          console.log(this.state.groups)
-      }
-      )
-   }
 
-   renderGroups = groups => {
+    console.log(user);
+    fetch(`${process.env.REACT_APP_API_URL}/groupsbyTags`, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(res => res.json())
+      .then(jsonData => {
+        //console.log(jsonData);
+        this.setState({ groups: jsonData });
+        console.log(this.state.groups);
+      });
+  }
+
+  renderGroups = groups => {
     return (
       <div className="col">
         {groups.map((group, i) => {
@@ -84,32 +80,32 @@ class Home extends Component{
                 </div>
               </Link>
             </div>
-
           );
         })}
-        
       </div>
     );
   };
 
-  render(){
-      return(
-        <>
-      <div>
-        <Menu/>
-      </div>
-      <div>
-      <Container>
-        <Row>
-      <Col xl = {3} >{ /* insert event and calender related function here */}</Col>
-      <Col xl = {6}>{ /* insert latests posts here */}</Col>
-          <Col xl = {3}>{this.renderGroups(this.state.groups)}</Col>
-        </Row>
-      </Container>
-      </div>
+  render() {
+    return (
+      <>
+        <div>
+          <Menu />
+        </div>
+        <div>
+          <Container>
+            <Row>
+              <Col xl={3}>
+                {/* insert event and calender related function here */}
+              </Col>
+              <Col xl={6}>{/* insert latests posts here */}</Col>
+              <Col xl={3}>{this.renderGroups(this.state.groups)}</Col>
+            </Row>
+          </Container>
+        </div>
       </>
-      );
-    }
+    );
+  }
 }
 
 export default Home;
